95cad0041e80bbc614fd228f265735168516e4c8
Gael237 <al07001861@tecmilenio.mx>
2026-05-05 20:40:40 -0600

    cambios en la ip

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL apuntada a la API en AWS
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://18.232.161.77:3000',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Inyecta el token JWT en cada request automáticamente
